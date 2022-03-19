89877f06fa95ee70f5cd64b98b0224d44478ef07
emalderete <nicolas.alderete95@live.com>
2022-03-18 23:18:46 -0300

    Pantalla de inicio (maquetado)
    Completado de la pantalla principal.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='mainContainer'>
             <div className='mainIcon'>
                 <img className='mainIconSvg' src={cinema}></img>
             </div>
