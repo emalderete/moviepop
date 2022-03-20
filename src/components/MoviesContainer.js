69f74aff513eb4a9ac9252913cfd83e0277d3574
emalderete <nicolas.alderete95@live.com>
2022-03-20 00:00:56 -0300

    Trabajo sobre maquetado de la página Movies

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -4,7 +4,22 @@ import cinema from '../img/cinema.svg';
 const MoviesContainer = () => {
     return (
         <div>
-           
+            <h6 className='movieTitle'>Películas</h6>
+            <hr/>
+            <div className='movieContainer'>
+                <div className='movieSlab'>
+                    <img src={cinema} alt=''></img>
+                </div>
+                <div className='movieSlab'>
+                    <img src={cinema} alt=''></img>
+                </div>
+                <div className='movieSlab'>
+                    <img src={cinema} alt=''></img>
+                </div>
+                <div className='movieSlab'>
+                    <img src={cinema} alt=''></img>
+                </div>
+            </div>
         </div>
     );
 };
